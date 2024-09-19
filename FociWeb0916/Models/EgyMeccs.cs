bbd3f065310665d68b0c6d86dc8caa19abb4d897
Csanad <you@example.com>
2024-09-19 09:07:27 +0200

    Sztédzs

---
+++ FociWeb0916/Models/EgyMeccs.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace FociWeb0916.Models
+{
+    public class EgyMeccs
+    {
+       
+    }
+}
--- FociWeb0916/obj/Debug/net8.0/FociWeb0916.AssemblyInfo.cs
+++ FociWeb0916/obj/Debug/net8.0/FociWeb0916.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FociWeb0916")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a2f4b7de40fe680f15e9bd0e74e0d84a1c06e7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed2eada0313cf21232d53bc2c216d4a1f067f7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FociWeb0916")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FociWeb0916")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
